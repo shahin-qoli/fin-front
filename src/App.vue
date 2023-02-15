ec10c55aef6f1114daafa96936d4a677e2873d01
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-02-15 11:03:18 +0330

    template added

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,13 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    </v-navigation-drawer>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-
     <v-main>
       <router-view/>
     </v-main>
@@ -49,7 +20,7 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: null
   }),
 };
 </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
