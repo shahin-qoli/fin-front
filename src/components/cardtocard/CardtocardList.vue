ed465744befd9c0137b74cd9234cbdd77e52db0b
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-03-15 11:47:10 +0330

    request list improved

--- src/components/cardtocard/CardtocardList.vue
+++ src/components/cardtocard/CardtocardList.vue
@@ -84,6 +84,7 @@ import TheRow from '../TheRow.vue'
     return {
       options: {
         itemsPerPage: 10,
+        page:1,
         transactioDate: "",
         amount: "",
         fromCard: "",
@@ -100,9 +101,7 @@ import TheRow from '../TheRow.vue'
   },watch:{
     options:{
       handler(){
-      
       this.loadCardtocards();    
- 
       },  deep: true
     }
   },
@@ -126,7 +125,6 @@ import TheRow from '../TheRow.vue'
           value: "description",
           align: "center",
           filterable: false
-
         },
         {
           text: "مبلغ",
@@ -186,6 +184,7 @@ import TheRow from '../TheRow.vue'
       async clearForm(){
           this.options =  {
         itemsPerPage: 10,
+        page:1,
         transactioDate: "",
         amount: "",
         fromCard: "",
