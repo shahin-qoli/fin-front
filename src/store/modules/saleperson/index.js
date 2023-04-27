3d0c9d60ca0270b4b443dbd7afa626dcef4fff0c
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-04-27 17:41:26 +0330

    going to list of sale persons

---
+++ src/store/modules/saleperson/index.js
@@ -0,0 +1,43 @@
+import { finAgent } from "@/services/agent";
+
+export default {
+    state(){
+        return {
+        salePersons: []
+    }
+    },
+    mutations:{
+        setSalePersons(state, payload){
+            state.salePersons = payload;
+        }
+    },
+    getters:{
+        getSalePersons(state){
+            return state.salePersons;
+        }
+    },
+    actions:{
+        async loadSalePersons(context){
+            try{
+                context.commit('setIsLoading', 'true')
+                const {data: salePersonData} = await finAgent.get('/front/sale_persons');
+                const salePersons = []
+                for (const item of salePersonData) {
+                    const salePerson= {
+                        ...item
+                    }
+                    salePersons.push(salePerson); 
+                }
+                context.commit('setIsLoading', 'false')
+                context.commit('setSalePersons', salePersons)
+
+            } catch (err) {
+                //console.log(err.response);
+                const error = new Error(
+                    err.response.data.error || 'Failed to fetch'
+                );
+                throw error;
+             } 
+        }
+    }
+}
