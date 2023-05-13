4048742b073505e04c371e10019a134597ad9c52
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-05-13 20:40:04 +0330

    to server

--- src/components/saleperson/SalePersonList.vue
+++ src/components/saleperson/SalePersonList.vue
@@ -54,39 +54,6 @@
                         </v-card-actions>
                     </v-card>
                     </v-dialog>
-                    <v-dialog v-model="dialogEdit" max-width="500px">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                        color="primary"
-                        dark
-                        class="mb-2"
-                        v-bind="attrs"
-                        v-on="on"
-                        >جدید</v-btn>
-                    </template>
-                    <v-card>
-                        <v-card-title>
-                        <span class="headline"><span class="text-h5">{{ formTitle }}</span></span>
-                        </v-card-title>
-                        <v-card-text>
-                        <v-container>
-                            <v-row>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.name" label="نام ویزیتور"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.b1_slpcode" label="کد بی وان"></v-text-field>
-                            </v-col>
-                            </v-row>
-                        </v-container>
-                        </v-card-text>
-                        <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="close">لغو</v-btn>
-                        <v-btn color="blue darken-1" text @click="save">ذخیره</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                    </v-dialog>
                 </v-toolbar>
             </template>
             <template v-slot:[`item.actions`]="{ item }">
