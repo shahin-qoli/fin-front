0ba25188b72f8a13016558b169f9cac00f73e143
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-08-22 15:44:33 +0330

    debug acc2acc

--- src/store/modules/accounttoaccount/index.js
+++ src/store/modules/accounttoaccount/index.js
@@ -64,10 +64,7 @@ export default{
             context.commit('setIsLoading', 'true')
             try {
                 console.log(payload)
-                const {data:responseData} = await finAgent.get(`/front/account_to_account_raws?\
-                page=${payload.page}&per_page=${payload.itemsPerPage}&q[transaction_date_eq]=${payload.transactionDate}\
-                &q[amount_eq]=${payload.amount}&q[peygiri_number_matches]=${payload.peygiriNumber}\
-                &q[is_used_eq]=${payload.isUsed}&q[bank_account_id_eq]=${payload.bankAccountId}`)
+                const {data:responseData} = await finAgent.get(`/front/account_to_account_raws?page=${payload.page}&per_page=${payload.itemsPerPage}&q[transaction_date_eq]=${payload.transactionDate}&q[amount_eq]=${payload.amount}&q[peygiri_number_matches]=${payload.peygiriNumber}&q[is_used_eq]=${payload.isUsed}&q[bank_account_id_eq]=${payload.bankAccountId}`)
                 var accounttoaccountsData = responseData.data;
                 var itemCount = responseData.options.count;
                 const accounttoaccounts = [];
