13bd9428f1f8da2d74e5ffe574f7113b48235b33
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-09-09 17:25:25 +0330

    init sync v2

---
+++ src/components/sync/SyncDocumentDetails.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-card>
+                <v-card-title>
+                    <h1>Document Details Page</h1>
+                </v-card-title>
+                <v-card-text>
+                    <v-row>
+                        <v-col 
+                        v-for="(document, index) in selectedDocs"
+                        :key="index"
+                        cols="12"
+                        >
+                        <v-card  @click="showDocumentDetails(document)">
+                            <v-card-text>{{ document }}</v-card-text>
+                        </v-card>   
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+  
+  <script>
+
+  </script>
+  
