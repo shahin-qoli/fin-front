53ff8afbc5db28d14aca19e950818902e1678c3f
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-10-26 01:15:12 +0330

    .

--- src/components/club/ClubMain.vue
+++ src/components/club/ClubMain.vue
@@ -24,42 +24,66 @@
                 <v-row>
                     <v-col cols="4">
                 <v-text-field
-                :value="reportData.points.point1"
+                :value="reportData.points.point1.point"
                 label="امتیاز 1 میلیون خرید نقدی"
                 disabled
                 ></v-text-field>
             </v-col>
             <v-col cols="4">
                 <v-text-field
-                :value="reportData.points.point2"
+                :value="reportData.points.point2.point"
                 label="امتیاز 1 میلیون خرید"
                 disabled
                 ></v-text-field>
             </v-col>
             <v-col cols="4">
                 <v-text-field
-                :value="reportData.points.point3"
-                label="امتیاز خرید محصول بالای 2 میلیون تومن"
+                :value="reportData.points.point2.point + reportData.points.point1.point"
+                label="جمع امتیاز"
                 disabled
                 ></v-text-field>
             </v-col>
             <v-col cols="6">
-                <v-text-field
-                :value="reportData.data.order"
-                label="جزییات فاکتورها"
-                disabled
-                ></v-text-field>
+                <v-card>
+                    <v-card-title>
+                        محاسبات امتیاز اول
+                    </v-card-title>
+                    <v-card-text>
+                       <p> کل مبلغ خرید شما {{ reportData.points.point1.summation | formatAmount}} ریال بوده است.</p>
+                       <p>با توجه به محاسبه هر {{ reportData.points.point1.divider | formatAmount}} یک امتیاز شما {{  reportData.points.point1.point }} امتیاز برده اید.</p>
+                    </v-card-text>
+                </v-card>
             </v-col>
             <v-col cols="6">
-                <v-text-field
-                :value="reportData.data.lines"
-                label="جزییات خط فاکتورها"
-                disabled
-                ></v-text-field>
+                <v-card>
+                    <v-card-title>
+                        محاسبات امتیاز دوم
+                    </v-card-title>
+                    <v-card-text>
+                       <p> کل مبلغ خرید نقدی شما {{ reportData.points.point2.summation | formatAmount }} ریال بوده است.</p>
+                       <p>با توجه به محاسبه هر {{ reportData.points.point2.divider | formatAmount }} دو امتیاز شما {{  reportData.points.point2.point }} امتیاز برده اید.</p>
+                    </v-card-text>
+                </v-card>
             </v-col>
-            <v-col cols="6">
+            <v-col cols="12">
                 <v-btn color="red" @click="reportData = null,reportData=null,mobileNumber=''" >بستن</v-btn>
             </v-col>
+            <v-card>
+                <v-data-table
+                fixed-header
+                dense
+                :headers="headers"
+                :items="reportData.data.order"
+                class="elevation-1"
+                >
+                <template v-slot:[`item.Total`]="{ item }">
+                <p>{{ item.Total | formatAmount }}</p>
+                </template>
+                <template v-slot:[`item.DocDate`]="{ item }">
+                <p>{{ item.DocDate | formatDate }}</p>
+                </template>
+                </v-data-table>
+            </v-card>
             </v-row>
             </v-card-text>
         </v-card>
@@ -84,6 +108,7 @@
 </template>
 
 <script>
+var jalaali = require('jalaali-js')
 export default {
     data(){
         return {
@@ -111,8 +136,64 @@ export default {
         }
     },
     computed:{
-
+        headers(){
+            return [
+                {
+                text: "شماره",
+                align: "center",
+                //sortable: false,
+                value: "DocNum",
+                },
+                {
+                text: "کد مشتری",
+                align: "center",
+                //sortable: false,
+                value: "CardCode",
+                },                {
+                text: "تاریخ",
+                align: "center",
+                //sortable: false,
+                value: "DocDate",
+                },                {
+                text: "وضعیت",
+                align: "center",
+                //sortable: false,
+                value: "DocStatus",
+                },                {
+                text: "جمع",
+                align: "center",
+                //sortable: false,
+                value: "Total",
+                },                {
+                text: "روش پرداخت",
+                align: "center",
+                //sortable: false,
+                value: "U_Settle_Type",
+                },
+                {
+                text: "زمان پرداخت",
+                align: "center",
+                //sortable: false,
+                value: "U_PayDueDate",
+                },
+            ]
+        }
     },
+    filters:{
+    formatAmount(value){
+      const stringVlue = String(value)
+      const formattedIntegerPart = stringVlue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      return formattedIntegerPart
+    },
+        formatDate(geoDate){
+            
+            var date = new Date(geoDate);
+            let jdate = jalaali.toJalaali(date.getFullYear(), date.getMonth()+1, date.getDate())
+            return `${jdate.jy}/${jdate.jm}/${jdate.jd}`
+        }
+    }
+    
 
 }
+
 </script>
