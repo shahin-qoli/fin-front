4f3935ee75d2d86f1182a0a5d2ad8a3a1bec8783
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-10-30 00:44:29 +0330

    updated

--- src/components/club/ClubMain.vue
+++ src/components/club/ClubMain.vue
@@ -1,98 +1,173 @@
 <template>
     <v-container>
-        <v-card outlined>
-            <v-card-text>
-                
-                    <v-form @submit.prevent="submitForm">
-                        <v-row>
-                        <v-col cols="5">
+        <v-card>
+            <v-tabs
+            @change="refreshData"
+            v-model="tab"
+            align-with-title
+            ><v-tabs-slider color="yellow"></v-tabs-slider>
+                <v-tab>
+                    <p>جستجو با شماره موبایل</p>
+                </v-tab>
+                <v-tab>
+                    <p>جستجو با شماره مشتری</p>
+                </v-tab>
+            </v-tabs>
+            <v-tabs-items 
+            v-model="tab">
+                <v-tab-item>
+                    <v-card class="section section_dark ma-5 pa-5" outlined>
+                        <v-card-text>  
+                                <v-form @submit.prevent="submitFormMobile">
+                                    <v-row>
+                                    <v-col cols="5">
+                                        <v-text-field
+                                        label="شماره موبایل"
+                                        v-model="mobileNumber"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-btn :loading="isLoading" type="submit" color="purple">جستجو</v-btn>
+                                    </v-col>
+                                </v-row>
+                                </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card class="section section_dark ma-5 pa-5" outlined>
+                        <v-card-text>  
+                                <v-form @submit.prevent="submitFormCode">
+                                    <v-row>
+                                    <v-col cols="5">
+                                        <v-text-field
+                                        label="شماره مشتری"
+                                        v-model="cardCode"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-btn :loading="isLoading" type="submit" color="purple">جستجو</v-btn>
+                                    </v-col>
+                                </v-row>
+                                </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-card>
+        <v-card class="section section_dark ma-5 pa-5" v-if="reportData">
+            <v-card class="section section_dark ma-5 pa-5">
+                <v-card-title>
+                    <p>مشخصات مشتری</p>
+                </v-card-title>
+                <v-card-text >
+                    <v-row>
+                        <v-col cols="3">
                             <v-text-field
-                            label="شماره موبایل"
-                            v-model="mobileNumber"
-                            ></v-text-field>
+                    :value="reportData.customer_data.cardName"
+                    label="نام مشتری"
+                    disabled
+                    ></v-text-field>
                         </v-col>
-                        <v-col cols="4">
-                            <v-btn :loading="isLoading" type="submit" color="purple">جستجو</v-btn>
+                        <v-col cols="3">
+                            <v-text-field
+                    :value="reportData.customer_data.cardCode"
+                    label="کد مشتری"
+                    disabled
+                    ></v-text-field>
+                        </v-col>
+                        <v-col cols="3">
+                            <v-text-field
+                    :value="reportData.customer_data.city"
+                    label="شهر "
+                    disabled
+                    ></v-text-field>
+                        </v-col>
+                        <v-col cols="3">
+                            <v-text-field
+                    :value="reportData.customer_data.cellular"
+                    label="شماره موبایل "
+                    disabled
+                    ></v-text-field>
                         </v-col>
                     </v-row>
-                    </v-form>
-                
-            </v-card-text>
-        </v-card>
-        <v-card v-if="reportData">
-            <v-card-text>
-                <v-row>
-                    <v-col cols="4">
-                <v-text-field
-                :value="reportData.points.point1.point"
-                label="امتیاز خرید"
-                disabled
-                ></v-text-field>
-            </v-col>
-            <v-col cols="4">
-                <v-text-field
-                :value="reportData.points.point2.point"
-                label="امتیاز نقدی"
-                disabled
-                ></v-text-field>
-            </v-col>
-            <v-col cols="4">
-                <v-text-field
-                :value="reportData.points.point2.point + reportData.points.point1.point"
-                label="جمع امتیاز"
-                disabled
-                ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-                <v-card>
-                    <v-card-title>
-                        محاسبات امتیاز اول
-                    </v-card-title>
-                    <v-card-text>
-                       <p> کل مبلغ خرید شما {{ reportData.points.point1.summation | formatAmount}} ریال بوده است.</p>
-                       <p>با توجه به محاسبه هر {{ reportData.points.point1.divider | formatAmount}} یک امتیاز شما {{  reportData.points.point1.point }} امتیاز کسب کرده اید.</p>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="6">
-                <v-card>
-                    <v-card-title>
-                        محاسبات امتیاز دوم
-                    </v-card-title>
-                    <v-card-text>
-                       <p> کل مبلغ خرید نقدی شما {{ reportData.points.point2.summation | formatAmount }} ریال بوده است.</p>
-                       <p>با توجه به محاسبه هر {{ reportData.points.point2.divider | formatAmount }} دو امتیاز شما {{  reportData.points.point2.point }} امتیاز کسب کرده اید.</p>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="12">
-            <v-card>
-                <v-card-title>
-                    <p>جزییات فاکتور</p>
-                </v-card-title>
-                <v-card-text>
-                <v-data-table
-                fixed-header
-                dense
-                :headers="headers"
-                :items="reportData.data.order"
-                class="elevation-1"
-                >
-                <template v-slot:[`item.Total`]="{ item }">
-                <p>{{ item.Total | formatAmount }}</p>
-                </template>
-                <template v-slot:[`item.DocDate`]="{ item }">
-                <p>{{ item.DocDate | formatDate }}</p>
-                </template>
-                </v-data-table>
-            </v-card-text>
+                </v-card-text>
             </v-card>
-        </v-col>
-            <v-col cols="12">
-                <v-btn color="red" @click="reportData = null,reportData=null,mobileNumber=''" >بستن</v-btn>
+            <v-card>
+                <v-card-text class="section section_dark">
+                    <v-row>
+                        <v-col cols="4">
+                    <v-text-field
+                    :value="reportData.points.point1.point"
+                    label="امتیاز خرید"
+                    disabled
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="4">
+                    <v-text-field
+                    :value="reportData.points.point2.point"
+                    label="امتیاز نقدی"
+                    disabled
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="4">
+                    <v-text-field
+                    :value="reportData.points.point2.point + reportData.points.point1.point"
+                    label="جمع امتیاز"
+                    disabled
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                    <v-card>
+                        <v-card-title>
+                            محاسبات امتیاز اول
+                        </v-card-title>
+                        <v-card-text>
+                        <p> کل مبلغ خرید شما {{ reportData.points.point1.summation | formatAmount}} ریال بوده است.</p>
+                        <p>با توجه به محاسبه هر {{ reportData.points.point1.divider | formatAmount}} یک امتیاز شما {{  reportData.points.point1.point }} امتیاز کسب کرده اید.</p>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="6">
+                    <v-card>
+                        <v-card-title>
+                            محاسبات امتیاز دوم
+                        </v-card-title>
+                        <v-card-text>
+                        <p> کل مبلغ خرید نقدی شما {{ reportData.points.point2.summation | formatAmount }} ریال بوده است.</p>
+                        <p>با توجه به محاسبه هر {{ reportData.points.point2.divider | formatAmount }} دو امتیاز شما {{  reportData.points.point2.point }} امتیاز کسب کرده اید.</p>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="12">
+                <v-card>
+                    <v-card-title>
+                        <p>جزییات فاکتور</p>
+                    </v-card-title>
+                    <v-card-text>
+                    <v-data-table
+                    fixed-header
+                    dense
+                    :headers="headers"
+                    :items="reportData.data.order"
+                    class="elevation-1"
+                    >
+                    <template v-slot:[`item.Total`]="{ item }">
+                    <p>{{ item.Total | formatAmount }}</p>
+                    </template>
+                    <template v-slot:[`item.DocDate`]="{ item }">
+                    <p>{{ item.DocDate | formatDate }}</p>
+                    </template>
+                    </v-data-table>
+                </v-card-text>
+                </v-card>
             </v-col>
-            </v-row>
-            </v-card-text>
+                <v-col cols="12">
+                    <v-btn color="red" @click="reportData = null,reportData=null,mobileNumber=''" >بستن</v-btn>
+                </v-col>
+                </v-row>
+                </v-card-text>
+            </v-card>
         </v-card>
         <v-dialog v-model="errorModal" max-width="500">
             <v-card>
@@ -119,25 +194,47 @@ var jalaali = require('jalaali-js')
 export default {
     data(){
         return {
+            tab: null,
             mobileNumber: '',
-            reportData:null,
+            cardCode: '',
+            reportData: null,
             errorData: null,
             errorModal: false,
             isLoading: false
         }
     },
     methods:{
-        submitForm(){
+        refreshData() {
+            console.log("Refreshing data...");
+            this.reportData = null;
+            this.mobileNumber = '';
+            this.cardCode = '';
+        },
+        submitFormMobile(){
             this.isLoading = true
             this.reportData = null
-            this.$store.dispatch('findClubReport', this.mobileNumber).then((response) => {
+            this.$store.dispatch('findClubReportMobile', this.mobileNumber).then((response) => {
                 this.isLoading = false
                 console.log(response)
                 if (response.success){
                     this.reportData = response.result
                 }
                 else{                   
-                    this.errorData = "شماره وارد شده صحیح نیست"
+                    this.errorData = response.result.error || "شماره وارد شده صحیح نیست"
+                    this.errorModal= true}
+            })
+        },
+        submitFormCode(){
+            this.isLoading = true
+            this.reportData = null
+            this.$store.dispatch('findClubReportCode', this.cardCode).then((response) => {
+                this.isLoading = false
+                console.log(response)
+                if (response.success){
+                    this.reportData = response.result
+                }
+                else{                   
+                    this.errorData =  response.result.error || "کد وارد شده صحیح نیست"
                     this.errorModal= true}
             })
         }
--- src/store/modules/club/index.js
+++ src/store/modules/club/index.js
@@ -5,10 +5,10 @@ export default{
     mutations:{},
     getters:{},
     actions:{
-        async findClubReport(context, payload){
+        async findClubReportMobile(context, payload){
             try{
             const body = {mobile_number: payload}
-            const response = await finAgent.post(`/v2/club/calculate`, body)  
+            const response = await finAgent.post(`/v2/club/calculate_by_mobile_number`, body)  
 
             if (response.status == 200)
                 return  {success: true, result:response.data}
@@ -21,5 +21,22 @@ export default{
             // console.log(err)
             return  {success: false, result: err.response.data}
          } 
-    }
+    },
+    async findClubReportCode(context, payload){
+        try{
+        const body = {card_code: payload}
+        const response = await finAgent.post(`/v2/club/calculate_by_cardcode`, body)  
+
+        if (response.status == 200)
+            return  {success: true, result:response.data}
+        else 
+            return  {success: false, result:response.data}
+        
+    }catch (err) {
+        //console.log(err.response);
+        // console.log("ssssssssssssssssssssssssssss")
+        // console.log(err)
+        return  {success: false, result: err.response.data}
+     } 
+}
 }}
