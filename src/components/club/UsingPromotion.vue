9a828f79b7e05841bc7f211204cac3e58191a377
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2023-11-14 19:26:36 +0330

    .

--- src/components/club/UsingPromotion.vue
+++ src/components/club/UsingPromotion.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
+    <v-container>
         <v-card>
-            <v-from submit.perevent="submit">
             <v-row class="ma-0" align="center">
                 <v-col cols="4">
-                    <v-text-filed label="کد مشتری" v-model="cardCode"></v-text-filed>
+                    <v-text-field class="border" label="کد مشتری" v-model="cardCode"></v-text-field>
                 </v-col>
                 <v-col cols="4">
-                    <v-text-filed label="شماره سفارش" v-model="b1doc"></v-text-filed>
+                    <v-text-field label="شماره سفارش" v-model="b1doc"></v-text-field>
                 </v-col>
                 <v-col cols="4">
-                    <v-text-filed label="کد تخفیف" v-model="PromoCode"></v-text-filed>
+                    <v-text-field label="کد تخفیف" v-model="PromoCode"></v-text-field>
                 </v-col>
                 <v-col cols="4">
-                    <v-btn type="submit"></v-btn>
+                    <v-btn @click="submit">ثبت</v-btn>
                 </v-col>
             </v-row>
-        </v-from>
         </v-card>
         <v-dialog v-model="okModal" max-width="500">
             <v-card>
@@ -25,7 +23,7 @@
                     <v-row>
                         <v-col cols="12">                
                             <div class="modal-scroll">
-                            <p>با موفقیت انجام شد</p>
+                            <p>{{ errorMessage }}</p>
                             </div>
                         </v-col>
                     </v-row>
@@ -35,7 +33,7 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
-    </div>
+    </v-container>
 </template>
 
 
@@ -56,9 +54,11 @@ export default{
     methods: {
         async submit(){
             try { 
-                payload ={b1doc: this.b1doc, used_by: this.cardCode,code: this.PromoCode}
-                const response = await finAgent.post(`/v2/promotion_code/use_coupon`,payload).then((response) => {
-                    if (response.status ==  200){
+                let payload ={b1doc: this.b1doc, used_by: this.cardCode,code: this.PromoCode}
+                await finAgent.post(`/v2/promotion_code/use_coupon`,payload).then((response) => {
+                    console.log("data");
+                    console.log(response.data);
+                    if (response.data.state > 0){
                         this.okModal = true;
                         this.errorMessage= "با موفقیت انجام شد."
                     }else{
