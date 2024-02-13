a961282a7621ec349fb27ba959e56369179c455c
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2024-02-13 23:56:13 +0330

    .

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -175,7 +175,7 @@
                   <v-expansion-panel-header class="grey lighten-5">
                   <h3>داشبورد فروش</h3>
                   </v-expansion-panel-header>
-                  <v-expansion-panel-content class="grey lighten-5">
+                  <v-expansion-panel-content v-if="transactionsAccess" class="grey lighten-5">
                     <v-col cols="12">
                     <router-link style="text-decoration: none;" to='/financialdasboard'><h5>Sale Order</h5></router-link> 
                     </v-col>
