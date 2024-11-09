2c10138368a9d781ec27f40d851601f4317d6866
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2024-11-09 10:28:49 +0330

    .

--- src/mixins/TransactionTypes.js
+++ src/mixins/TransactionTypes.js
@@ -5,13 +5,15 @@ export const TransactionTypes ={
                 ["0", "نامشخص"],
                 ["1", "کارت به کارت"],
                 ["2", "حساب به حساب"],
-                ["3", "وصول چک"]
+                ["3", "وصول چک"],
+                ["4","پرداخت پل"]
             ]),
             transactionTypesColor: new Map([
                 ["0", "red"],
                 ["1", "blue"],
                 ["2", "green"],
                 ["3", "yellow"],
+                ["4","purple"]
             ])
 
         }
